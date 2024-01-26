35996d066f07c9b4178d454c6e223a365a891023
Ankush <ankushgt25@gmail.com>
2024-01-26 15:31:19 +0530

    ejs added

--- 10-Ejs/4.0+EJS/index.js
+++ 10-Ejs/4.0+EJS/index.js
@@ -3,6 +3,30 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/",(req,res)=>{
+    const today = new Date();
+    //Test code
+    // weekend:
+    // new Date("June 24, 2023 11:13:00");
+    // weekday:
+    // new Date("June 20, 2023 11:13:00");
+    const day = today.getDay();
+
+    console.log(day);
+
+    let type = "a weekday";
+    let adv = "its time to work hard";
+    if (day===0 || day ===6) {
+        type= "the weekend!";
+        adv = "its time to work hard";
+        
+    }
+    res.render("index.ejs",{
+        dayType : "a week day",
+        advice: "Its time to work hard",
+    });
+});
+
 app.listen(port, ()=>{
     console.log(`Server is running at port ${port}`);
 });
